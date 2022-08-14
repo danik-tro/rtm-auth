c6b4aeedfac938665646f0d48f899fc2f5970f33
Daniil Trotsenko <daniil.trotsenko.dev@gmail.com>
2022-08-14 21:32:38 +0300

    feat: Implement simple web-server

---
+++ src/handlers.rs
@@ -0,0 +1,18 @@
+use actix_web::Responder;
+
+
+pub async fn get_users() -> impl Responder {
+    format!("Hello from get users")
+}
+
+pub async fn get_user_by_id() -> impl Responder {
+    format!("Hello from get users by id")
+}
+
+pub async fn add_user() -> impl Responder {
+    format!("Hello from add user")
+}
+
+pub async fn delete_user() -> impl Responder {
+    format!("Hello from delete user")
+}
