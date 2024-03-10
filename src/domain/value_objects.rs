8e65af5d4012f305bd3fe64ac92778bf284baa8e
Daniil Trotsenko <daniil.trotsenko@icloud.com>
2024-03-10 17:07:01 +0200

    fix: Remove nutype crate

--- src/domain/value_objects.rs
+++
@@ -1,8 +0,0 @@
-use nutype::nutype;
-
-#[nutype(
-    sanitize(trim, lowercase),
-    validate(not_empty, len_char_max = 255),
-    derive(Debug, PartialEq, PartialOrd, Clone)
-)]
-pub struct Username(String);
